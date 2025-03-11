41e97744224cab3fcdbe5461ac5cb63255df8c3c
718355 <718355@student.dusd.net>
2025-03-11 08:57:45 -0700

    Done.

---
+++ HernandezCreatePropP7/Assets/Scripts/ExtensionMethods/SomeClasssE.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SomeClasssE : MonoBehaviour
+{
+    void Start()
+    {
+            transform.ResetTransformation();
+    }
+}
